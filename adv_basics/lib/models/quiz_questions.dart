fcc0d90bb9fc4dae5c9186486ff75dfb5310ea55
Francisco Andre <franciscodamana@gmail.com>
2024-04-15 09:39:58 +0200

    Mutating Values in Memory

--- adv_basics/lib/models/quiz_questions.dart
+++ adv_basics/lib/models/quiz_questions.dart
@@ -3,4 +3,10 @@ class QuizQuestion {
 
   final String text;
   final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    final shufledList = List.of(answers);
+    shufledList.shuffle();
+    return shufledList;
+  }
 }

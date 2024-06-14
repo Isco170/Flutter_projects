66d1366d786d2fbd3e555b9e0e932e51de9692b3
Francisco Andre <franciscodamana@gmail.com>
2024-06-14 09:40:35 +0200

    Using Theme Data in Widgets

--- expense_tracer/lib/widgets/expenses_list/expense_item.dart
+++ expense_tracer/lib/widgets/expenses_list/expense_item.dart
@@ -15,11 +15,13 @@ class ExpenseItem extends StatelessWidget {
           vertical: 16,
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(expense.title),
-            const SizedBox(
-              height: 4,
+            Text(
+              expense.title,
+              style: Theme.of(context).textTheme.titleLarge,
             ),
+            const SizedBox(height: 4),
             Row(
               children: [
                 Text('\$${expense.amount.toStringAsFixed(2)}'),
@@ -27,9 +29,7 @@ class ExpenseItem extends StatelessWidget {
                 Row(
                   children: [
                     Icon(categoryIcons[expense.category]),
-                    const SizedBox(
-                      height: 8,
-                    ),
+                    const SizedBox(height: 8),
                     Text(expense.formattedDate),
                   ],
                 ),

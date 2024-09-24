3aa286034eb41ad824344fd818a21a63afc1a72d
Isco170 <franciscodamana@gmail.com>
2024-09-24 14:31:34 +0200

    Adding Tab-based Navigation

---
+++ meals/lib/screens/tabs.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:meals/screens/categories.dart';
+import 'package:meals/screens/meals.dart';
+
+class TabsSreen extends StatefulWidget {
+  const TabsSreen({super.key});
+
+  @override
+  State<TabsSreen> createState() {
+    return _TabsSreen();
+  }
+}
+
+class _TabsSreen extends State<TabsSreen> {
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activePage = const CategoriesScreen();
+    var activePageTitle = 'Categories';
+
+    if (_selectedPageIndex == 1) {
+      activePage = MealsScreen(meals: []);
+      activePageTitle = 'Your Favorities';
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(activePageTitle),
+      ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          ),
+        ],
+      ),
+    );
+  }
+}
